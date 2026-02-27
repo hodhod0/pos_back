ce3a23a392bbd8448f9fcadd356fb83a03edfee5
hodod <alameddinehodhod@gmail.com>
2026-02-27 19:57:33 +0200

    Removed ai_router and updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import item_router, category_router, auth_router,ai_router,user_router
+from routers import item_router, category_router, auth_router,user_router
 app = FastAPI(title="POS Backend")
 
 # ------------------------------
@@ -25,7 +25,6 @@ app.add_middleware(
 app.include_router(item_router.router)
 app.include_router(category_router.router)
 app.include_router(auth_router.router)
-app.include_router(ai_router.router)
 app.include_router(user_router.router)
 
 @app.get("/")
